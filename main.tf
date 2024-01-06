7506f8bed723dcfb810cc514a2698c7fc7d264be
Vaishnavi123Deokar <149041783+Vaishnavi123Deokar@users.noreply.github.com>
2024-01-06 14:47:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,9 @@ secret_key = var.AWS_SECRET_KEY
 
 region = "us-east-1"
 
-} resource "aws_instance" "myec2" {
+} 
+
+resource "aws_instance" "myec2" {
 
   ami           = "ami-079db87dc4c10ac91"
 
